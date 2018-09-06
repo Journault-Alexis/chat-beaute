0bb63e712d7b74072f1cdce3e447a331bd802b54
Alexis Journault <journaultalexis@gmail.com>
2018-09-07 01:10:29 +0200

    mongodb localhost connection

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,11 @@
 
+
+require('./startup/db')();
+
 const express = require('express');
 const app = express();
+
+
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Chat Beauté on port ${port}...`));
