694ed1f4b909a2688f9b824e2bcaae2c56304ef6
Alexis Journault <journaultalexis@gmail.com>
2018-09-07 17:34:53 +0200

    angular project creation

--- backend/routes/cats.js
+++ backend/routes/cats.js
@@ -1,42 +1,43 @@
-const {Cat, validate} = require('../models/cat'); 
+const { Cat, validate } = require('../models/cat');
 const mongoose = require('mongoose');
 const express = require('express');
 const router = express.Router();
 
-router.get('/all', 
-    async function getAllCatsbyDescendingScore (req, res) {
-      const cats = await Cat
+router.get('/all',
+  async function getAllCatsbyDescendingScore(req, res) {
+    const cats = await Cat
       .find()
-      .sort({score: -1})   
-      res.send(cats);
-})
+      .sort({ score: -1 })
+    res.send(cats);
+  })
 
-router.get('/random', 
-    async function getRandomCat (req, res) {
-      const numberOfCats = await Cat.find().countDocuments() 
-      const randomNumber = Math.floor(Math.random() * numberOfCats)
-      const randomCat = await Cat.find().limit(1).skip(randomNumber)
-      res.send(randomCat);
-})
+router.get('/random',
+  async function getRandomCat(req, res) {
+    const numberOfCats = await Cat.find().countDocuments()
+    const randomNumber = Math.floor(Math.random() * numberOfCats)
+    const randomCat = await Cat.find().limit(1).skip(randomNumber)
+    res.send(randomCat);
+  })
 
-router.post('/', 
-    async function fillTheDB (req, res) {
-      for (var i = 0; i<req.body.length ; i++) {
-        let cat = new Cat({ 
+router.post('/',
+  async function fillTheDB(req, res) {
+    for (var i = 0; i < req.body.length; i++) {
+      let cat = new Cat({
         image: req.body[i].image,
         score: req.body[i].score,
-    });
-    let cats = await cat.save();
-  }})
+      });
+      let cats = await cat.save();
+    }
+  })
 
-  router.put('/', 
-      async function increaseScore (req, res)  {
-        const cat = await Cat.findById(req.query.id);
-          cat.score ++
-          const catWithScoreIncrement  = await cat.save();
+router.put('/',
+  async function increaseScore(req, res) {
+    const cat = await Cat.findById(req.query.id);
+    cat.score++
+    const catWithScoreIncrement = await cat.save();
     if (!cat) return res.status(404).send('Oups, the cat with the given ID was not found.');
-    
-  });  
- 
+
+  });
+
 
 module.exports = router; 
